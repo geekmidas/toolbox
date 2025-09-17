bbdfc093cc234fb24e2b8f6e1695eedbe45a8bf8
geekmidas <lebogang@technanimals.com>
2025-09-17 17:56:03 +0200

    :goal_net: chore(api): events error

--- packages/api/src/constructs/publisher.ts
+++ packages/api/src/constructs/publisher.ts
@@ -10,44 +10,50 @@ export async function publishEndpointEvents<
   response: EndpointOutput<T>,
   serviceDiscovery: ServiceDiscovery<any, any>,
 ) {
-  if (!endpoint.events?.length) {
-    endpoint.logger.debug('No events to publish');
-    return;
-  }
-
-  if (!endpoint.publisherService) {
-    endpoint.logger.warn('No publisher service available');
-    return;
-  }
-
-  // Register the service and get the instance
-  const services = await serviceDiscovery.register([endpoint.publisherService]);
-
-  const publisher = services[
-    endpoint.publisherService.serviceName
-  ] as EventPublisher<any>;
-
-  const events: any[] = [];
-
-  for (const { when, payload, type, ...e } of endpoint.events) {
-    endpoint.logger.debug({ event: type }, 'Processing event');
-    const resolvedPayload = await payload(response);
-    const event = {
-      ...e,
-      type,
-      payload: resolvedPayload,
-    };
-
-    if (!when || when(response as any)) {
-      events.push(event);
+  try {
+    if (!endpoint.events?.length) {
+      endpoint.logger.debug('No events to publish');
+      return;
     }
-  }
 
-  if (events.length) {
-    endpoint.logger.debug({ eventCount: events.length }, 'Publishing events');
+    if (!endpoint.publisherService) {
+      endpoint.logger.warn('No publisher service available');
+      return;
+    }
 
-    await publisher.publish(events).catch((err: any) => {
-      endpoint.logger.error(err, 'Failed to publish events');
-    });
+    // Register the service and get the instance
+    const services = await serviceDiscovery.register([
+      endpoint.publisherService,
+    ]);
+
+    const publisher = services[
+      endpoint.publisherService.serviceName
+    ] as EventPublisher<any>;
+
+    const events: any[] = [];
+
+    for (const { when, payload, type, ...e } of endpoint.events) {
+      endpoint.logger.debug({ event: type }, 'Processing event');
+      const resolvedPayload = await payload(response);
+      const event = {
+        ...e,
+        type,
+        payload: resolvedPayload,
+      };
+
+      if (!when || when(response as any)) {
+        events.push(event);
+      }
+    }
+
+    if (events.length) {
+      endpoint.logger.debug({ eventCount: events.length }, 'Publishing events');
+
+      await publisher.publish(events).catch((err) => {
+        endpoint.logger.error(err, 'Failed to publish events');
+      });
+    }
+  } catch (error) {
+    endpoint.logger.error(error, 'Something went wrong publishing events');
   }
 }
