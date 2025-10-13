7a36d20cda5ae17e32b64e21c5837dfaa7c365a2
geekmidas <lebogang@technanimals.com>
2025-10-13 16:14:25 +0200

    :memo: docs: fix example project

--- apps/example/src/config/env.ts
+++ apps/example/src/config/env.ts
@@ -1,8 +1,10 @@
 import { EnvironmentParser } from '@geekmidas/envkit';
 
+const testExchange = 'geekmidas_events_example';
+
 const envParser = new EnvironmentParser({
   ...process.env,
-  EVENT_SUBSCRIBER_CONNECTION_STRING: 'basic://in-memory',
+  EVENT_SUBSCRIBER_CONNECTION_STRING: `rabbitmq://geekmidas:geekmidas@localhost:5672?exchange=${testExchange}&autoConnect=true`,
 });
 
 export default envParser;
