dff15bb27ff114599eec5004e00f3fbfd0bd6d0e
geekmidas <lebogang@technanimals.com>
2025-10-22 13:24:06 +0200

    :heavy_plus_sign: chore: new version

--- apps/example/src/endpoints/router.ts
+++ apps/example/src/endpoints/router.ts
@@ -1,4 +1,8 @@
 import { e } from '@geekmidas/constructs/endpoints';
+import { ConsoleLogger } from '@geekmidas/logger/console';
 import { EventsService } from '../services/EventsService';
 
-export const router = e.publisher(EventsService);
+export const router = e
+  .logger(new ConsoleLogger())
+  .services([])
+  .publisher(EventsService);
--- packages/constructs/src/endpoints/index.ts
+++ packages/constructs/src/endpoints/index.ts
@@ -1,5 +1,13 @@
 import { EndpointFactory } from './EndpointFactory';
 
-export { Endpoint, type EndpointOutput, type EndpointSchemas } from './Endpoint';
+export { EndpointFactory } from './EndpointFactory';
+export {
+  Endpoint,
+  type EndpointOutput,
+  type EndpointSchemas,
+  type EndpointHandler,
+  type EndpointContext,
+} from './Endpoint';
+export { EndpointBuilder } from './EndpointBuilder';
 
 export const e = new EndpointFactory();
