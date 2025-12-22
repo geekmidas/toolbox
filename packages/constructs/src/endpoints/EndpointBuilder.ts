f5f86b979f77b2f099c4cdc592ae46c45e7bfefc
geekmidas <lebogang@technanimals.com>
2025-12-22 14:08:45 +0200

    🐛 fix: handle missing authorizer in available list gracefully

--- packages/constructs/src/endpoints/EndpointBuilder.ts
+++ packages/constructs/src/endpoints/EndpointBuilder.ts
@@ -616,8 +616,10 @@ export class EndpointBuilder<
     TDatabaseServiceName
   > {
     // Find authorizer metadata if name is set
+    // If the authorizer name is set but not in availableAuthorizers, create a simple authorizer object
     const authorizer = this._authorizerName
-      ? this._availableAuthorizers.find((a) => a.name === this._authorizerName)
+      ? this._availableAuthorizers.find((a) => a.name === this._authorizerName) ??
+        { name: this._authorizerName }
       : undefined;
 
     return new Endpoint({
