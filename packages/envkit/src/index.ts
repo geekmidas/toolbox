4a3b9babbac1e0dd7bdb89c78f8438eb6fac6502
geekmidas <lebogang@technanimals.com>
2025-12-22 23:48:41 +0200

    📦 chore: Export EnvironmentBuilder and related types from main index

--- packages/envkit/src/index.ts
+++ packages/envkit/src/index.ts
@@ -1,1 +1,11 @@
 export { EnvironmentParser, ConfigParser } from './EnvironmentParser';
+export {
+  EnvironmentBuilder,
+  environmentCase,
+  type EnvRecord,
+  type EnvValue,
+  type EnvironmentResolver,
+  type Resolvers,
+  type EnvironmentBuilderOptions,
+  type InputValue,
+} from './EnvironmentBuilder';
