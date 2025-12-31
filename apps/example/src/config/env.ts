b9616a9977ac5925f45fd74a35c7b08ccb8691ae
geekmidas <lebogang@technanimals.com>
2025-12-31 18:17:46 +0200

    🔧 refactor: update env.ts export style for named imports

--- apps/example/src/config/env.ts
+++ apps/example/src/config/env.ts
@@ -2,9 +2,7 @@ import { EnvironmentParser } from '@geekmidas/envkit';
 
 const testExchange = 'geekmidas_events_example';
 
-const envParser = new EnvironmentParser({
+export const envParser = new EnvironmentParser({
   ...process.env,
   EVENT_SUBSCRIBER_CONNECTION_STRING: `rabbitmq://geekmidas:geekmidas@localhost:5672?exchange=${testExchange}&autoConnect=true`,
 });
-
-export default envParser;
