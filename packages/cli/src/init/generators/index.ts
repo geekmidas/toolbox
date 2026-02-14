d04a6bd08bca7323fd967366e751a336e5b76c9e
geekmidas <lebogang@technanimals.com>
2026-02-14 06:59:44 +0200

    📦 Export test generator from generators index
    Adds the test file generator to the public exports of the generators module, making it available for use in the initialization command.

--- packages/cli/src/init/generators/index.ts
+++ packages/cli/src/init/generators/index.ts
@@ -3,3 +3,4 @@ export { generateConfigFiles } from './config.js';
 export { generateEnvFiles } from './env.js';
 export { generatePackageJson } from './package.js';
 export { generateSourceFiles } from './source.js';
+export { generateTestFiles } from './test.js';
