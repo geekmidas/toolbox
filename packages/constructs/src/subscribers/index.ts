d977ade3bb00cc839518bcedb17e379bae8495a7
geekmidas <lebogang@technanimals.com>
2026-02-18 00:00:14 +0200

    🗑️ Remove unused TestSubscriberAdaptor export

--- packages/constructs/src/subscribers/index.ts
+++ packages/constructs/src/subscribers/index.ts
@@ -2,6 +2,5 @@ import { SubscriberBuilder } from './SubscriberBuilder';
 
 export { Subscriber } from './Subscriber';
 export { SubscriberBuilder };
-export { TestSubscriberAdaptor } from './TestSubscriberAdaptor';
 
 export const s = new SubscriberBuilder();
