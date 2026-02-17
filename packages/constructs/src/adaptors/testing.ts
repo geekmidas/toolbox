6efb51861d19d428e213cdb75efa0747e1d5e273
geekmidas <lebogang@technanimals.com>
2026-02-17 10:31:40 +0200

    ✨ Export TestFunctionAdaptor from testing adaptors barrel

--- packages/constructs/src/adaptors/testing.ts
+++ packages/constructs/src/adaptors/testing.ts
@@ -1,2 +1,3 @@
 export { TestEndpointAdaptor } from '../endpoints/TestEndpointAdaptor';
+export { TestFunctionAdaptor } from '../functions/TestFunctionAdaptor';
 export { TestSubscriberAdaptor } from '../subscribers/TestSubscriberAdaptor';
